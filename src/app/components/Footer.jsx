8a75489a0c3e4489f1f9e58e1199a491696474f7
pranaya_pradhan <saiju.pranu@gmail.com>
2025-04-20 01:03:51 +0545

    Logo removed

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -6,10 +6,11 @@ import Logo from "../../../public/logo1.png"
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-      <div className="container p-4 flex justify-between">
-      <Link href={"/"}>
-            <Image src={Logo} alt="Logo Icon" />
-          </Link>
+      <div className="container p-12 flex justify-between">
+        {/* <Link href={"/"}>
+          <Image src={Logo} alt="Logo Icon" />
+        </Link> */}
+        <span>PRANAYA</span>
         <p className="text-slate-600">&copy; 2025 Pranaya Sainju - All rights reserved.</p>
       </div>
     </footer>
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -28,15 +28,15 @@ const Navbar = () => {
   return (
     <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
-        {/* <Link
+        <Link
           href={"/"}
           className="text-2xl md:text-5xl text-white font-semibold"
         >
-          LOGO
-        </Link> */}
-        <Link href={"/"}>
+          PRANAYA
+        </Link>
+        {/* <Link href={"/"}>
             <Image src={Logo} alt="Logo Icon" />
-          </Link>
+          </Link> */}
         <div className="mobile-menu block md:hidden">
           {!navbarOpen ? (
             <button
