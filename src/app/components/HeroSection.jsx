92142dedf36ba288d05051f07e67b0dd1ff6d886
pranaya_pradhan <saiju.pranu@gmail.com>
2025-04-20 02:00:44 +0545

    All changes done

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -64,7 +64,7 @@ const HeroSection = () => {
         >
           <div className="rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative"> 
             <Image
-              src="./images/hero.png"
+              src="/images/hero.png"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={390}
